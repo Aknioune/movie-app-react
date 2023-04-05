62489bd24fca6f115f3dded164faa9320849eced
Aknioune <Fatimazaknioune@gmail.com>
2023-04-05 13:13:26 +0000

    reactHooks

---
+++ src/components/MovieCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
+
+
+
+const MovieCard = ({ movie }) => {
+  return (
+  <div className='movie'>
+<div className="MovieCard" key={movie.id}>
+  <img src={movie.posterURL} alt={movie.title} />
+  <div className="MovieCardDetails">
+  <h3>{movie.title}</h3>
+    <p className="MovieCardRating">Rating: {movie.rating}</p>
+  </div>
+  <div className="MovieCardDescription">
+    <p>{movie.description}</p>
+    <div className="MovieCardPlay">
+      <FontAwesomeIcon icon={faPlay}/>
+    </div>
+  </div>
+</div>
+</div>
+
+  );
+};
+
+export default MovieCard;
